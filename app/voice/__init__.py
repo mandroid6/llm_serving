09efaac55e61def77b4afcfb4479963ae042a919
Mandar Deshpande <mandarde@meta.com>
2025-08-14 16:05:22 -0700

    resolve issues for VoiceInputMode unavailable

--- app/voice/__init__.py
+++ app/voice/__init__.py
@@ -2,12 +2,13 @@
 Voice input module for speech-to-text integration with chat interface
 """
 
-from .manager import VoiceInputManager
+from .manager import VoiceInputManager, VoiceInputMode
 from .recorder import VoiceRecorder
 from .transcriber import WhisperTranscriber
 
 __all__ = [
     "VoiceInputManager",
+    "VoiceInputMode",
     "VoiceRecorder", 
     "WhisperTranscriber"
 ]
